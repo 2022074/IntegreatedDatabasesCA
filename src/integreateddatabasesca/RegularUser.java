27332af4e987d1785b832ddbed9987fe070723e7
2022332 <2022332@student.cct.ie>
2023-12-21 23:26:44 +0000

    Updating User
    Updating the attributes and deleting the enums to change the userTypes from enums to boolean

--- src/integreateddatabasesca/RegularUser.java
+++ src/integreateddatabasesca/RegularUser.java
@@ -9,7 +9,7 @@ package integreateddatabasesca;
  * @author Ari
  */
 public class RegularUser extends Users{
-    public RegularUser(String username, String password, String name, String surname) {
-        super(username, password, name, surname, UserType.REGULAR_USER);
+    public RegularUser(String username, String password, String firstName, String lastName, String gender, String email, String marital_status, boolean if_married_both_work, boolean children, int id, int employeeID, boolean userType) {
+        super(username, password, firstName, lastName, gender,email, marital_status, if_married_both_work, children, id, employeeID, userType);
     }
 }
