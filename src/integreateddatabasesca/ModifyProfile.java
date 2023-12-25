b25a159e4ff0dd5f39a605dc31b15d1a47ac545b
2022332 <2022332@student.cct.ie>
2023-12-25 21:22:39 +0000

    Update and create
    Did some changes in users because I decided to create a new class called modifyProfile to update the data from the database. Also changed and commented GUI and database. For Gui I implemented the new method that I created to modify the profile and commenting the code. And in the database I created the new method for the modify profiles and also commented the code.

---
+++ src/integreateddatabasesca/ModifyProfile.java
@@ -0,0 +1,65 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package integreateddatabasesca;
+
+/**
+ *
+ * @author Ari
+ */
+public class ModifyProfile {
+    private int id;
+    private String username;
+    private String password;
+    private String firstName;
+    private String lastName;
+
+    public ModifyProfile(int id, String username, String password, String firstName, String lastName) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
